084a0aa58cb7c8b33c55f2ce128a31dee258c0a9
Drzn8 <abhishekdarsan@gmail.com>
2026-05-30 01:12:26 +0530

    color revamped resume and links added

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
+
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+
+    function opengit() {
+  window.open("https://github.com/drzn8", "_blank");
+}
+    function download() {
+        const link = document.createElement('a');
+        link.href = "./MyResume.pdf";
+        link.download = 'Abhishek_Darsan_CV.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
